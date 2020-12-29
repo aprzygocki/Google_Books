b5e8e18bb317cb1e59179598a0247f107ace36ab
aprzygocki <augustino.przygocki@yahoo.com>
2020-12-29 02:05:23 -0800

    reloaded package.json both client and root

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer>
       <hr />
       <p className="pull-right">
-        <i className="fab fa-github" /> Proudly built by Katrina Arando using React.js
+        <i className="fab fa-github" /> Proudly built by <a href="https://augustino.us" target="_blank">Augustino Przygocki</a> using React.js
       </p>
     </footer>
   );
